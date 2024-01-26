bb4254f81baa315dd035bc8b25a29a6338c42086
Aaron Lal <lucidlear@gmail.com>
2024-01-27 09:16:49 +1100

    props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,13 @@
-import Button from "./Button.jsx";
+import Student from "./Student";
 
 function App() {
   return (
     <>
-      <Button />
+    <Student name="Spongebob" age="30" isStudent={true} />
+    <Student name="Patrik" age={42} isStudent={false} />
+    <Student name="Squidward" age={50} isStudent={false} />
+    <Student name="Sandy" age={27} isStudent={true} />
+    <Student name="Larry" />
     </>
   );
 }
