addf8447bc4e175b039061a70ec69b1032a7088d
Aaron <aaronlal61@gmail.com>
2024-02-17 12:57:25 +1100

    finished up render lists

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,28 @@
-import List from './List'
+import List from "./List";
 
 function App() {
+  const fruits = [
+    { id: 1, name: "apple", calories: "apple" },
+    { id: 2, name: "orange", calories: 45 },
+    { id: 3, name: "bananna", calories: 105 },
+    { id: 4, name: "coconut", calories: 159 },
+    { id: 5, name: "pineapple", calories: 37 },
+  ];
+
+  const vegetables = [
+    { id: 6, name: "potatoes", calories: 110 },
+    { id: 7, name: "celery", calories: 15 },
+    { id: 8, name: "carrots", calories: 25 },
+    { id: 9, name: "corn", calories: 63 },
+    { id: 10, name: "broccoli", calories: 50 },
+  ];
+
   return (
     <>
-    <List />
+      {fruits.length > 0 && <List category="Fruits" />}
+      {vegetables.length > 0 ? (
+        <List items={vegetables} category="Vegetables" />
+      ) : null}
     </>
   );
 }
