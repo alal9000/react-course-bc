700b9a01fb96dfb13736d546f77eec9f56ca0163
Aaron Lal <lucidlear@gmail.com>
2024-02-23 06:51:23 +1100

    finished update array of objects in state

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,10 @@
-import MyComponent5 from './MyComponent5'
+import MyComponent6 from './MyComponent6'
 
 
 function App() {
 
 
-  return (<MyComponent5 />);
+  return (<MyComponent6 />);
 }
 
 export default App;
