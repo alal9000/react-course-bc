60e3b5192246a4f1f738474435dcc2f4a536f9a6
Aaron Lal <lucidlear@gmail.com>
2024-02-24 17:15:41 +1100

    finished up stopwatch app

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,11 @@
 // you may need to adjust css because we are sharing one file for all components and projects
 
-import MyComponent8 from "./MyComponent8";
+import StopWatch from "./StopWatch/StopWatch";
 
 function App() {
   return (
     <>
-      <MyComponent8 />
+      <StopWatch />
     </>
   );
 }
